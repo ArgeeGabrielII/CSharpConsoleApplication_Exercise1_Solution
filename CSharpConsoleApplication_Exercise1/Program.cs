7b41d356ed1508548be532fb86c01502f7d9ef9c
Rosalito Gabriel II <rosalito.gabriel@gmail.com>
2019-01-29 23:32:13 +0800

    Create a C# Console Application that prints a "Hello" on the first line then "<YourName> on the second line.

--- CSharpConsoleApplication_Exercise1/Program.cs
+++ CSharpConsoleApplication_Exercise1/Program.cs
@@ -10,6 +10,16 @@ namespace CSharpConsoleApplication_Exercise1
     {
         static void Main(string[] args)
         {
+            //Create a C# Console Application that prints a "Hello" on the first line then "<YourName> on the second line.
+            //Let's Begin! :)
+
+            Console.WriteLine("Hello"); //This prints a "Hello" on the first line.
+            Console.WriteLine("Argee"); //This prints "<My Name>" on the second line.
+            Console.ReadKey(); //This waits for a keypress from the user, so that it would exit the console.
+
+            //Let's run the Console Application by Pressing F5 or clicking the Start Button on top.
+
+            //That's it! You have done your first programming with C# Console.
         }
     }
 }
